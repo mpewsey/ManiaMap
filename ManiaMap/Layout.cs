76f9081ff1adbfc6f59f8824e70533da05b6042a
Matt Pewsey <23442063+mpewsey@users.noreply.github.com>
2022-02-07 17:22:42 -0500

    Add LayoutGenerator

--- ManiaMap/Layout.cs
+++ ManiaMap/Layout.cs
@@ -9,66 +9,32 @@ namespace ManiaMap
     public class Layout
     {
         public int Seed { get; }
+        public int MaxRebases { get; }
+        public int MaxBranchLength { get; }
+        public int Rebases { get; private set; }
         public Dictionary<int, Room> Rooms { get; } = new();
         public List<DoorConnection> DoorConnections { get; } = new();
 
-        public Layout(int seed)
+        public Layout(int seed, int maxRebases, int maxBranchLength)
         {
             Seed = seed;
+            MaxRebases = maxRebases;
+            MaxBranchLength = maxBranchLength;
         }
 
         public Layout(Layout baseLayout)
         {
             Seed = baseLayout.Seed;
+            MaxRebases = baseLayout.Seed;
+            MaxBranchLength = baseLayout.MaxBranchLength;
             Rooms = new(baseLayout.Rooms);
             DoorConnections = new(baseLayout.DoorConnections);
+            baseLayout.Rebases++;
         }
 
         public override string ToString()
         {
             return $"Layout(Seed = {Seed})";
-        }
-
-        public static Layout Generate(int seed, LayoutGraph graph, TemplateGroups groups, int maxRebases = 10000, int maxBranchLength = -1)
-        {
-            int chain = 0;
-            var random = new Random(seed);
-            var spaces = groups.GetConfigurationSpaces();
-            var chains = new GraphChainDecomposer(graph, maxBranchLength).FindChains();
-            var rebases = new Stack<int>();
-            var layouts = new Stack<Layout>();
-            layouts.Push(new(seed));
-            rebases.Push(0);
-
-            while (layouts.Count > 0)
-            {
-                var baseLayout = layouts.Peek();
-                var rebase = rebases.Pop();
-
-                if (chain >= chains.Count)
-                {
-                    return baseLayout;
-                }
-
-                if (rebase >= maxRebases)
-                {
-                    layouts.Pop();
-                    chain--;
-                    continue;
-                }
-
-                rebases.Push(rebase + 1);
-                var layout = new Layout(baseLayout);
-                
-                if (layout.AddChain(chains[chain], graph, groups, spaces, random))
-                {
-                    rebases.Push(0);
-                    layouts.Push(layout);
-                    chain++;
-                }
-            }
-
-            return null;
         }
 
         public bool Intersects(RoomTemplate template, int dx, int dy)
@@ -77,122 +43,6 @@ namespace ManiaMap
             {
                 if (template.Intersects(room.Template, room.X - dx, room.Y - dy))
                 {
-                    return true;
-                }
-            }
-
-            return false;
-        }
-
-        public bool AddChain(List<LayoutEdge> chain, LayoutGraph graph, TemplateGroups groups,
-            Dictionary<TemplatePair, ConfigurationSpace> spaces, Random random)
-        {
-            foreach (var edge in chain)
-            {
-                if (!AddEdge(edge, graph, groups, spaces, random))
-                {
-                    return false;
-                }
-            }
-
-            return true;
-        }
-
-        private bool AddEdge(LayoutEdge edge, LayoutGraph graph, TemplateGroups groups,
-            Dictionary<TemplatePair, ConfigurationSpace> spaces, Random random)
-        {
-            var fromExists = Rooms.TryGetValue(edge.FromNode, out var fromRoom);
-            var toExists = Rooms.TryGetValue(edge.ToNode, out var toRoom);
-
-            if (fromExists && toExists)
-            {
-                return AddDoorConnection(edge, random);
-            }
-
-            if (!fromExists && !toExists)
-            {
-                var fromNode = graph.GetNode(edge.FromNode);
-                var toNode = graph.GetNode(edge.ToNode);
-
-                return AddFirstRoom(fromNode, groups, random)
-                    && AddRoom(Rooms[edge.FromNode], toNode, edge.Direction, groups, spaces, random);
-            }
-
-            if (!fromExists)
-            {
-                var node = graph.GetNode(edge.FromNode);
-                var direction = Door.ReverseEdgeDirection(edge.Direction);
-                return AddRoom(toRoom, node, direction, groups, spaces, random);
-            }
-
-            if (!toExists)
-            {
-                var node = graph.GetNode(edge.ToNode);
-                return AddRoom(fromRoom, node, edge.Direction, groups, spaces, random);
-            }
-
-            return false;
-        }
-
-        private bool AddRoom(Room room, LayoutNode node, EdgeDirection direction, TemplateGroups groups,
-            Dictionary<TemplatePair, ConfigurationSpace> spaces, Random random)
-        {
-            var templates = groups.GetTemplates(node.TemplateGroups);
-            Statistics.Shuffle(templates, random);
-            
-            foreach (var template in templates)
-            {
-                var space = spaces[new(room.Template, template)];
-                var configurations = Statistics.Shuffled(space.Configurations, random);
-
-                foreach (var config in configurations)
-                {
-                    var x = config.X + room.X;
-                    var y = config.Y + room.Y;
-
-                    if (config.EdgeDirection == direction && !Intersects(template, x, y))
-                    {
-                        var newRoom = new Room(node.Id, x, y, template);
-                        var connection = new DoorConnection(room, newRoom, config.FromDoor, config.ToDoor);
-                        Rooms.Add(node.Id, newRoom);
-                        DoorConnections.Add(connection);
-                        return true;
-                    }
-                }
-            }
-
-            return false;
-        }
-
-        private bool AddFirstRoom(LayoutNode node, TemplateGroups templateGroups, Random random)
-        {
-            var templates = templateGroups.GetTemplates(node.TemplateGroups);
-            Statistics.Shuffle(templates, random);
-
-            foreach (var template in templates)
-            {
-                if (!Intersects(template, 0, 0))
-                {
-                    Rooms.Add(node.Id, new(node.Id, 0, 0, template));
-                    return true;
-                }
-            }
-
-            return false;
-        }
-
-        private bool AddDoorConnection(LayoutEdge edge, Random random)
-        {
-            var from = Rooms[edge.FromNode];
-            var to = Rooms[edge.ToNode];
-            var doorPairs = from.Template.AlignedDoors(to.Template, to.X - from.X, to.Y - from.Y);
-            Statistics.Shuffle(doorPairs, random);
-
-            foreach (var pair in doorPairs)
-            {
-                if (pair.EdgeDirection() == edge.Direction)
-                {
-                    DoorConnections.Add(new(from, to, pair.FromDoor, pair.ToDoor));
                     return true;
                 }
             }
---
+++ ManiaMap/LayoutGenerator.cs
@@ -0,0 +1,183 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ManiaMap
+{
+    public class LayoutGenerator
+    {
+        private int Seed { get; }
+        private int MaxRebases { get; }
+        private int MaxBranchLength { get; }
+        private LayoutGraph Graph { get; }
+        private TemplateGroups TemplateGroups { get; }
+        private Dictionary<TemplatePair, ConfigurationSpace> ConfigurationSpaces { get; set; }
+        private List<List<LayoutEdge>> Chains { get; set; }
+        private Random Random { get; set; }
+
+        public LayoutGenerator(int seed, LayoutGraph graph, TemplateGroups templateGroups, int maxRebases = 10000, int maxBranchLength = 3)
+        {
+            Seed = seed;
+            MaxRebases = maxRebases;
+            MaxBranchLength = maxBranchLength;
+            Graph = graph;
+            TemplateGroups = templateGroups;
+        }
+
+        public Layout GenerateLayout()
+        {
+            int chain = 0;
+            Random = new Random(Seed);
+            Chains = Graph.FindChains(MaxBranchLength);
+            ConfigurationSpaces = TemplateGroups.GetConfigurationSpaces();
+            var layouts = new Stack<Layout>();
+            layouts.Push(new(Seed, MaxRebases, MaxBranchLength));
+
+            while (layouts.Count > 0)
+            {
+                var baseLayout = layouts.Peek();
+
+                if (chain >= Chains.Count)
+                {
+                    return baseLayout;
+                }
+
+                if (baseLayout.Rebases >= MaxRebases)
+                {
+                    layouts.Pop();
+                    chain--;
+                    continue;
+                }
+
+                var layout = new Layout(baseLayout);
+
+                if (AddChain(layout, chain))
+                {
+                    layouts.Push(layout);
+                    chain++;
+                }
+            }
+
+            return null;
+        }
+
+        private bool AddChain(Layout layout, int index)
+        {
+            var chain = Chains[index];
+            
+            foreach (var edge in chain)
+            {
+                if (!AddEdge(layout, edge))
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+
+        private bool AddEdge(Layout layout, LayoutEdge edge)
+        {
+            var fromExists = layout.Rooms.TryGetValue(edge.FromNode, out var fromRoom);
+            var toExists = layout.Rooms.TryGetValue(edge.ToNode, out var toRoom);
+
+            if (!fromExists && !toExists)
+            {
+                var fromNode = Graph.GetNode(edge.FromNode);
+                var toNode = Graph.GetNode(edge.ToNode);
+                return AddFirstRoom(layout, fromNode) && AddRoom(layout, layout.Rooms[edge.FromNode], toNode, edge.Direction);
+            }
+
+            if (!fromExists)
+            {
+                var node = Graph.GetNode(edge.FromNode);
+                var direction = Door.ReverseEdgeDirection(edge.Direction);
+                return AddRoom(layout, toRoom, node, direction);
+            }
+
+            if (!toExists)
+            {
+                var node = Graph.GetNode(edge.ToNode);
+                return AddRoom(layout, fromRoom, node, edge.Direction);
+            }
+
+            return AddDoorConnection(layout, edge);
+        }
+
+        private bool AddRoom(Layout layout, Room room, LayoutNode node, EdgeDirection direction)
+        {
+            var templates = TemplateGroups.GetTemplates(node.TemplateGroups);
+            Shuffle(templates);
+
+            foreach (var template in templates)
+            {
+                var space = ConfigurationSpaces[new(room.Template, template)];
+                Shuffle(space.Configurations);
+
+                foreach (var config in space.Configurations)
+                {
+                    var x = config.X + room.X;
+                    var y = config.Y + room.Y;
+
+                    if (config.EdgeDirection == direction && !layout.Intersects(template, x, y))
+                    {
+                        var newRoom = new Room(node.Id, x, y, template);
+                        var connection = new DoorConnection(room, newRoom, config.FromDoor, config.ToDoor);
+                        layout.Rooms.Add(node.Id, newRoom);
+                        layout.DoorConnections.Add(connection);
+                        return true;
+                    }
+                }
+            }
+
+            return false;
+        }
+
+        private bool AddFirstRoom(Layout layout, LayoutNode node)
+        {
+            var templates = TemplateGroups.GetTemplates(node.TemplateGroups);
+            Shuffle(templates);
+
+            foreach (var template in templates)
+            {
+                if (!layout.Intersects(template, 0, 0))
+                {
+                    layout.Rooms.Add(node.Id, new(node.Id, 0, 0, template));
+                    return true;
+                }
+            }
+
+            return false;
+        }
+
+        private bool AddDoorConnection(Layout layout, LayoutEdge edge)
+        {
+            var from = layout.Rooms[edge.FromNode];
+            var to = layout.Rooms[edge.ToNode];
+            var doorPairs = from.Template.AlignedDoors(to.Template, to.X - from.X, to.Y - from.Y);
+            Shuffle(doorPairs);
+
+            foreach (var pair in doorPairs)
+            {
+                if (pair.EdgeDirection() == edge.Direction)
+                {
+                    layout.DoorConnections.Add(new(from, to, pair.FromDoor, pair.ToDoor));
+                    return true;
+                }
+            }
+
+            return false;
+        }
+
+        private void Shuffle<T>(List<T> list)
+        {
+            for (int i = 0; i < list.Count; i++)
+            {
+                var j = Random.Next(i, list.Count - 1);
+                (list[i], list[j]) = (list[j], list[i]);
+            }
+        }
+    }
+}
