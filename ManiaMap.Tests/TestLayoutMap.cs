ae11019fcf16b8bd8d21a243439af73ada97c104
Matt Pewsey <23442063+mpewsey@users.noreply.github.com>
2022-02-09 22:12:25 -0500

    Add LayoutMap

---
+++ ManiaMap.Tests/TestLayoutMap.cs
@@ -0,0 +1,64 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+
+namespace ManiaMap.Tests
+{
+    [TestClass]
+    public class TestLayoutMap
+    {
+        private static RoomTemplate GetSquareTemplate()
+        {
+            var o = new Cell();
+            var a = new Cell { LeftDoor = new(DoorType.TwoWay), TopDoor = new(DoorType.TwoWay) };
+            var b = new Cell { TopDoor = new(DoorType.TwoWay) };
+            var c = new Cell { TopDoor = new(DoorType.TwoWay), RightDoor = new(DoorType.TwoWay) };
+            var d = new Cell { LeftDoor = new(DoorType.TwoWay) };
+            var e = new Cell { RightDoor = new(DoorType.TwoWay) };
+            var f = new Cell { LeftDoor = new(DoorType.TwoWay), BottomDoor = new(DoorType.TwoWay) };
+            var g = new Cell { BottomDoor = new(DoorType.TwoWay) };
+            var h = new Cell { BottomDoor = new(DoorType.TwoWay), RightDoor = new(DoorType.TwoWay) };
+
+            var cells = new Cell[,]
+            {
+                { a, b, c },
+                { d, o, e },
+                { f, g, h },
+            };
+
+            return new(1, cells);
+        }
+
+        private static LayoutGraph GetCrossGraph()
+        {
+            var graph = new LayoutGraph(1);
+
+            graph.AddNode(0).TemplateGroups.Add("Default");
+            graph.AddNode(1).TemplateGroups.Add("Default");
+            graph.AddNode(2).TemplateGroups.Add("Default");
+            graph.AddNode(3).TemplateGroups.Add("Default");
+            graph.AddNode(4).TemplateGroups.Add("Default");
+            graph.AddNode(5).TemplateGroups.Add("Default");
+
+            graph.AddEdge(0, 1);
+            graph.AddEdge(1, 2);
+            graph.AddEdge(0, 3);
+            graph.AddEdge(0, 4);
+            graph.AddEdge(0, 5);
+
+            return graph;
+        }
+
+        [TestMethod]
+        public void TestSavePng()
+        {
+            var graph = GetCrossGraph();
+            var templateGroups = new TemplateGroups();
+            templateGroups.Add("Default", GetSquareTemplate());
+            var generator = new LayoutGenerator(0, graph, templateGroups);
+            var layout = generator.GenerateLayout();
+            var map = new LayoutMap(layout);
+            map.SavePng("Map.png");
+        }
+    }
+}
---
+++ ManiaMap/LayoutMap.cs
@@ -0,0 +1,156 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.Text;
+
+namespace ManiaMap
+{
+    public class LayoutMap
+    {
+        private Layout Layout { get; }
+        private int TileWidth { get; }
+        private int TileHeight { get; }
+        private int Padding { get; }
+        private Color BackgroundColor { get; } = Color.Black;
+        private Color CellBackgroundColor { get; } = Color.MidnightBlue;
+        private Dictionary<string, Bitmap> Tiles { get; }
+        private HashSet<RoomDoorPair> RoomDoors { get; } = new HashSet<RoomDoorPair>();
+        
+        public LayoutMap(Layout layout, int tileWidth = 16, int tileHeight = 16,
+            int padding = 1, Dictionary<string, Bitmap> tiles = null)
+        {
+            Layout = layout;
+            TileWidth = tileWidth;
+            TileHeight = tileHeight;
+            Padding = padding;
+            Tiles = tiles ?? MapTiles.GetDefaultTiles();
+        }
+
+        /// <summary>
+        /// Returns the rectangular bounds for the layout.
+        /// </summary>
+        private Rectangle LayoutBounds()
+        {
+            if (Layout.Rooms.Count == 0)
+                return new Rectangle();
+
+            var minX = int.MaxValue;
+            var minY = int.MaxValue;
+            var maxX = int.MinValue;
+            var maxY = int.MinValue;
+
+            foreach (var room in Layout.Rooms.Values)
+            {
+                minX = Math.Min(minX, room.X);
+                minY = Math.Min(maxY, room.Y);
+                maxX = Math.Max(maxX, room.X + room.Template.Cells.Rows);
+                maxY = Math.Max(maxY, room.Y + room.Template.Cells.Columns);
+            }
+
+            return new Rectangle(minY, minX, maxY - minY, maxX - minX);
+        }
+
+        /// <summary>
+        /// Adds the room door pairs for the layout to the room door set.
+        /// </summary>
+        private void MarkRoomDoors()
+        {
+            RoomDoors.Clear();
+
+            foreach (var connection in Layout.DoorConnections)
+            {
+                RoomDoors.Add(new RoomDoorPair(connection.FromRoom, connection.FromDoor));
+                RoomDoors.Add(new RoomDoorPair(connection.ToRoom, connection.ToDoor));
+            }
+        }
+
+        /// <summary>
+        /// Renders a map of the layout and saves it to the designated file path.
+        /// </summary>
+        public void SavePng(string path)
+        {
+            var map = CreateImage();
+            map.Save(path, ImageFormat.Png);
+        }
+
+        /// <summary>
+        /// Returns a rendered map of the layout.
+        /// </summary>
+        public Bitmap CreateImage()
+        {
+            MarkRoomDoors();
+            var bounds = LayoutBounds();
+            var width = TileWidth * (2 * Padding + bounds.Width);
+            var height = TileHeight * (2 * Padding + bounds.Height);
+            var map = new Bitmap(width, height);
+            var graphic = Graphics.FromImage(map);
+
+            // Add background fill
+            var backgroundBrush = new SolidBrush(BackgroundColor);
+            graphic.FillRectangle(backgroundBrush, 0, 0, width, height);
+
+            // Draw map tiles
+            var cellBrush = new SolidBrush(CellBackgroundColor);
+
+            foreach (var room in Layout.Rooms.Values)
+            {
+                var cells = room.Template.Cells;
+                var x0 = (room.Y - bounds.X + Padding) * TileWidth;
+                var y0 = (room.X - bounds.Y + Padding) * TileHeight;
+                
+                for (int i = 0; i < cells.Rows; i++)
+                {
+                    for (int j = 0; j < cells.Columns; j++)
+                    {
+                        var cell = cells[i, j];
+
+                        if (cell != null)
+                        {
+                            var x = TileWidth * j + x0;
+                            var y = TileHeight * i + y0;
+                            
+                            var top = cells.GetOrDefault(i - 1, j);
+                            var bottom = cells.GetOrDefault(i + 1, j);
+                            var left = cells.GetOrDefault(i, j - 1);
+                            var right = cells.GetOrDefault(i, j + 1);
+
+                            var topTile = GetTile(room, cell.TopDoor, top, "TopDoor", "TopWall");
+                            var bottomTile = GetTile(room, cell.BottomDoor, bottom, "BottomDoor", "BottomWall");
+                            var leftTile = GetTile(room, cell.LeftDoor, left, "LeftDoor", "LeftWall");
+                            var rightTile = GetTile(room, cell.RightDoor, right, "RightDoor", "RightWall");
+
+                            // Add cell background fill
+                            graphic.FillRectangle(cellBrush, x, y, TileWidth, TileHeight);
+
+                            // Superimpose applicable map tiles
+                            if (topTile != null)
+                                graphic.DrawImage(topTile, x, y);
+                            if (bottomTile != null)
+                                graphic.DrawImage(bottomTile, x, y);
+                            if (leftTile != null)
+                                graphic.DrawImage(leftTile, x, y);
+                            if (rightTile != null)
+                                graphic.DrawImage(rightTile, x, y);
+                        }
+                    }
+                }
+            }
+
+            return map;
+        }
+
+        /// <summary>
+        /// Returns the map tile corresponding to the wall or door location.
+        /// Returns null if the tile has neither a wall or door.
+        /// </summary>
+        private Bitmap GetTile(Room room, Door door, Cell neighbor, string doorName, string wallName)
+        {
+            if (door != null && RoomDoors.Contains(new RoomDoorPair(room, door)))
+                return Tiles[doorName];
+            if (neighbor == null)
+                return Tiles[wallName];
+            return null;
+        }
+    }
+}
