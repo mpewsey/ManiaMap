31306ce846092d685b51bbef44a7f7f8cf84009f
Matt Pewsey <23442063+mpewsey@users.noreply.github.com>
2022-02-14 00:01:52 -0500

    Update DoorPair.cs

--- ManiaMap/DoorPair.cs
+++ ManiaMap/DoorPair.cs
@@ -1,6 +1,9 @@
-﻿namespace MPewsey.ManiaMap
+﻿using System;
+using System.Collections.Generic;
+
+namespace MPewsey.ManiaMap
 {
-    public struct DoorPair
+    public struct DoorPair : IEquatable<DoorPair>
     {
         public Door FromDoor { get; }
         public Door ToDoor { get; }
@@ -16,12 +19,33 @@
             return $"DoorPair(FromDoor = {FromDoor}, ToDoor = {ToDoor})";
         }
 
-        /// <summary>
-        /// Returns the edge direction for the door pair.
-        /// </summary>
-        public EdgeDirection EdgeDirection()
+        public override bool Equals(object obj)
         {
-            return Door.GetEdgeDirection(FromDoor.Type, ToDoor.Type);
+            return obj is DoorPair pair && Equals(pair);
+        }
+
+        public bool Equals(DoorPair other)
+        {
+            return EqualityComparer<Door>.Default.Equals(FromDoor, other.FromDoor) &&
+                   EqualityComparer<Door>.Default.Equals(ToDoor, other.ToDoor);
+        }
+
+        public override int GetHashCode()
+        {
+            int hashCode = -1667591795;
+            hashCode = hashCode * -1521134295 + EqualityComparer<Door>.Default.GetHashCode(FromDoor);
+            hashCode = hashCode * -1521134295 + EqualityComparer<Door>.Default.GetHashCode(ToDoor);
+            return hashCode;
+        }
+
+        public static bool operator ==(DoorPair left, DoorPair right)
+        {
+            return left.Equals(right);
+        }
+
+        public static bool operator !=(DoorPair left, DoorPair right)
+        {
+            return !(left == right);
         }
     }
 }
