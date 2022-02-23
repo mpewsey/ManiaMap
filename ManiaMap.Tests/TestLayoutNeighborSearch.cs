592b89d484fd65a13810e1b816d7b75002cd7c89
Matt Pewsey <23442063+mpewsey@users.noreply.github.com>
2022-02-23 17:47:52 -0500

    Create TestLayoutNeighborSearch.cs

---
+++ ManiaMap.Tests/TestLayoutNeighborSearch.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
+
+namespace MPewsey.ManiaMap.Tests
+{
+    [TestClass]
+    public class TestLayoutNeighborSearch
+    {
+        [TestMethod]
+        public void TestFindNeighborsOfGeekGraph()
+        {
+            var graph = Samples.GraphLibrary.GeekGraph();
+
+            var templateGroups = new TemplateGroups();
+            templateGroups.Add("Default", Samples.TemplateLibrary.Miscellaneous.HyperSquareTemplate());
+
+            var generator = new LayoutGenerator(123456, graph, templateGroups);
+            var layout = generator.GenerateLayout();
+
+            var result = layout.FindNeighbors(5, 2);
+            var expected = new List<int>() { 5, 3, 2, 4, 6, 9, 10 };
+            CollectionAssert.AreEquivalent(expected, result);
+        }
+    }
+}
