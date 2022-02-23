84e2c2bcfd25c32a3334d8ac223b67776f85ee2d
Matt Pewsey <23442063+mpewsey@users.noreply.github.com>
2022-02-23 14:07:57 -0500

    Create TestLayout.cs

---
+++ ManiaMap.Tests/TestLayout.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace MPewsey.ManiaMap.Tests
+{
+    [TestClass]
+    public class TestLayout
+    {
+        [TestMethod]
+        public void TestSaveAndLoad()
+        {
+            var path = "ManiaMapLayout.xml";
+            var layout = Samples.ManiaMapSample.ManiaMapLayout();
+            layout.Save(path);
+            var copy = Layout.Load(path);
+            Assert.AreEqual(layout.Seed, copy.Seed);
+        }
+    }
+}
