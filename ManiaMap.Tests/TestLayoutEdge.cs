0dd549b2365160362727ed5befbecbf4d1420158
Matt Pewsey <23442063+mpewsey@users.noreply.github.com>
2022-02-23 18:28:51 -0500

    Create TestLayoutEdge.cs

---
+++ ManiaMap.Tests/TestLayoutEdge.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace MPewsey.ManiaMap.Tests
+{
+    [TestClass]
+    public class TestLayoutEdge
+    {
+        [TestMethod]
+        public void TestToString()
+        {
+            var edge = new LayoutEdge(1, 2);
+            Assert.IsTrue(edge.ToString().StartsWith("LayoutEdge("));
+        }
+
+        [TestMethod]
+        public void TestToSymbolString()
+        {
+            var edge = new LayoutEdge(1, 2);
+            edge.Direction = EdgeDirection.Both;
+            Assert.AreEqual("(1 <=> 2)", edge.ToSymbolString());
+            edge.Direction = EdgeDirection.ForwardFlexible;
+            Assert.AreEqual("(1 => 2)", edge.ToSymbolString());
+            edge.Direction = EdgeDirection.ForwardFixed;
+            Assert.AreEqual("(1 -> 2)", edge.ToSymbolString());
+            edge.Direction = EdgeDirection.ReverseFlexible;
+            Assert.AreEqual("(1 <= 2)", edge.ToSymbolString());
+            edge.Direction = EdgeDirection.ReverseFixed;
+            Assert.AreEqual("(1 <- 2)", edge.ToSymbolString());
+        }
+    }
+}
