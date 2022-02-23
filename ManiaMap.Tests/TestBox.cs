7ff4255f89274f423a314b6bc1e76d9b6964ceec
Matt Pewsey <23442063+mpewsey@users.noreply.github.com>
2022-02-23 14:19:33 -0500

    Update TestBox.cs

--- ManiaMap.Tests/TestBox.cs
+++ ManiaMap.Tests/TestBox.cs
@@ -20,5 +20,27 @@ namespace MPewsey.ManiaMap.Tests
             var shaft = new Box(8, 8, 10, 10, -10, 10);
             Assert.IsTrue(shaft.Intersects(square, 8, 10, 0));
         }
+
+        [TestMethod]
+        public void TestRangeIntersects()
+        {
+            var shaft = new Box(8, 8, 10, 10, -10, 10);
+            Assert.IsTrue(shaft.Intersects(8, 8, 10, 10, 0, 0));
+        }
+
+        [TestMethod]
+        public void TestRangeDoesNotIntersect()
+        {
+            var shaft = new Box(8, 8, 10, 10, -10, 10);
+            Assert.IsFalse(shaft.Intersects(0, 0, 0, 0, 0, 0));
+        }
+
+        [TestMethod]
+        public void TestToString()
+        {
+            var result = new Box(1, 2, 3, 4, 5, 6).ToString();
+            var expected = "Box(XMin = 1, XMax = 2, YMin = 3, YMax = 4, ZMin = 5, ZMax = 6)";
+            Assert.AreEqual(expected, result);
+        }
     }
 }
