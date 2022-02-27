85283b1d2588f582ede0310d732cb1acdfc1dd4f
Matt Pewsey <23442063+mpewsey@users.noreply.github.com>
2022-02-26 19:46:12 -0500

    Create TestUid.cs

---
+++ ManiaMap.Tests/TestUid.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace MPewsey.ManiaMap.Tests
+{
+    [TestClass]
+    public class TestUid
+    {
+        [TestMethod]
+        public void TestEqualsOperator()
+        {
+            var x = new Uid(1, 2, 3);
+            var y = new Uid(1, 2, 3);
+            Assert.IsTrue(x == y);
+        }
+
+        [TestMethod]
+        public void TestDoesNotEqualOperator()
+        {
+            var x = new Uid(1, 2, 3);
+            var y = new Uid(1, 2, 4);
+            Assert.IsTrue(x != y);
+        }
+    }
+}
