4f07175c0eb1f1354876518869b2abc278420a7e
Matt Pewsey <23442063+mpewsey@users.noreply.github.com>
2022-02-26 20:59:03 -0500

    Update TestUid.cs

--- ManiaMap.Tests/TestUid.cs
+++ ManiaMap.Tests/TestUid.cs
@@ -20,5 +20,24 @@ namespace MPewsey.ManiaMap.Tests
             var y = new Uid(1, 2, 4);
             Assert.IsTrue(x != y);
         }
+
+        [TestMethod]
+        public void TestInitializers()
+        {
+            var x = new Uid(1);
+            Assert.AreEqual(1, x.Value1);
+            Assert.AreEqual(0, x.Value2);
+            Assert.AreEqual(0, x.Value3);
+
+            var y = new Uid(1, 2);
+            Assert.AreEqual(1, y.Value1);
+            Assert.AreEqual(2, y.Value2);
+            Assert.AreEqual(0, y.Value3);
+
+            var z = new Uid(1, 2, 3);
+            Assert.AreEqual(1, z.Value1);
+            Assert.AreEqual(2, z.Value2);
+            Assert.AreEqual(3, z.Value3);
+        }
     }
 }
