9f640f7ece22b4b0846b9ed4240b9c6d0b2ce54f
Matt Pewsey <23442063+mpewsey@users.noreply.github.com>
2022-03-30 12:19:55 -0400

    Update RandomSeed.cs

--- src/ManiaMap/RandomSeed.cs
+++ src/ManiaMap/RandomSeed.cs
@@ -40,5 +40,46 @@ namespace MPewsey.ManiaMap
                 (list[i], list[j]) = (list[j], list[i]);
             }
         }
+
+        /// <summary>
+        /// Draws a random weighted index from an array.
+        /// </summary>
+        /// <param name="weights">An array of weights.</param>
+        public int DrawWeightedIndex(double[] weights)
+        {
+            if (weights.Length > 0)
+            {
+                var totals = CumSum(weights);
+                var value = totals[totals.Length - 1] * Random.NextDouble();
+
+                for (int i = 0; i < totals.Length; i++)
+                {
+                    if (value <= totals[i] && totals[i] > 0)
+                    {
+                        return i;
+                    }
+                }
+            }
+
+            return -1;
+        }
+
+        /// <summary>
+        /// Returns the cumulative sum of the array.
+        /// </summary>
+        /// <param name="values">An array of values.</param>
+        public static double[] CumSum(double[] values)
+        {
+            double total = 0;
+            var totals = new double[values.Length];
+            
+            for (int i = 0; i < values.Length; i++)
+            {
+                total += values[i];
+                totals[i] = total;
+            }
+
+            return totals;
+        }
     }
 }
