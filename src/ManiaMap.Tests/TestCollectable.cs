d8f0f5f7c4e92f4505eacc6806f8e338b01764d9
Matt Pewsey <23442063+mpewsey@users.noreply.github.com>
2022-03-31 10:18:34 -0400

    Add CollectableGroups

---
+++ src/ManiaMap.Tests/TestCollectable.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace MPewsey.ManiaMap.Tests
+{
+    [TestClass]
+    public class TestCollectable
+    {
+        [TestMethod]
+        public void TestEqualsOperator()
+        {
+            var x = new Collectable("Default", 1);
+            var y = new Collectable("Default", 1);
+            Assert.IsTrue(x == y);
+            Assert.IsFalse(x.Equals(null));
+        }
+
+        [TestMethod]
+        public void TestDoesNotEqualOperator()
+        {
+            var x = new Collectable("Default", 1);
+            var y = new Collectable("Default", 2);
+            Assert.IsTrue(x != y);
+        }
+    }
+}
---
+++ src/ManiaMap.Tests/TestCollectableGroups.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace MPewsey.ManiaMap.Tests
+{
+    [TestClass]
+    public class TestCollectableGroups
+    {
+        [TestMethod]
+        public void TestAdd()
+        {
+            var groups = new CollectableGroups();
+            var expected = new List<int> { 1, 2, 3 };
+
+            groups.Groups.Clear();
+            groups.Add("Default", 1);
+            groups.Add("Default", 2);
+            groups.Add("Default", 3);
+            CollectionAssert.AreEqual(expected, groups.Groups["Default"]);
+
+            groups.Groups.Clear();
+            groups.Add("Default", 1, 2, 3);
+            CollectionAssert.AreEqual(expected, groups.Groups["Default"]);
+
+            groups.Groups.Clear();
+            groups.Add("Default", new List<int> { 1, 2, 3 });
+            CollectionAssert.AreEqual(expected, groups.Groups["Default"]);
+
+            groups.Groups.Clear();
+            groups.Add("Default", new List<int> { 1, 2 }, new List<int> { 3 });
+            CollectionAssert.AreEqual(expected, groups.Groups["Default"]);
+        }
+
+        [TestMethod]
+        public void TestGetCollectables()
+        {
+            var groups = new CollectableGroups();
+            groups.Add("Default", 1);
+            var expected = new List<Collectable> { new Collectable("Default", 1) };
+            var result = groups.GetCollectables().ToList();
+            CollectionAssert.AreEqual(expected, result);
+        }
+
+        [TestMethod]
+        public void TestToString()
+        {
+            var groups = new CollectableGroups();
+            Assert.IsTrue(groups.ToString().StartsWith("CollectableGroups("));
+        }
+    }
+}
