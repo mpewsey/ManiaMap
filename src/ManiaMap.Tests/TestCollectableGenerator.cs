2ed1b158322a4d778e8e99f7e90b36d8b5006c75
Matt Pewsey <23442063+mpewsey@users.noreply.github.com>
2022-04-02 19:16:08 -0400

    Consolidate template groups for Big Layout

--- src/ManiaMap.Tests/TestCollectableGenerator.cs
+++ src/ManiaMap.Tests/TestCollectableGenerator.cs
@@ -10,56 +10,12 @@ namespace MPewsey.ManiaMap.Tests
         public void TestGenerateLayoutForBigLayout()
         {
             var graph = Samples.GraphLibrary.BigGraph();
-            var templateGroups = new TemplateGroups();
+            var templateGroups = Samples.BigLayoutSample.BigLayoutTemplateGroups();
+
             var collectableGroups = new CollectableGroups();
-
-            var square2x2 = Samples.TemplateLibrary.Squares.Square2x2Template();
-            var square3x3 = Samples.TemplateLibrary.Squares.Square3x3Template();
-            var rect2x3 = Samples.TemplateLibrary.Rectangles.Rectangle2x3Template();
-            var rect2x4 = Samples.TemplateLibrary.Rectangles.Rectangle2x4Template();
-
-            var square1x1 = Samples.TemplateLibrary.Squares.Square1x1Template();
-            var rect1x2 = Samples.TemplateLibrary.Rectangles.Rectangle1x2Template();
-            var rect1x3 = Samples.TemplateLibrary.Rectangles.Rectangle1x3Template();
-            var rect1x4 = Samples.TemplateLibrary.Rectangles.Rectangle1x4Template();
-            var angle3x4 = Samples.TemplateLibrary.Angles.Angle3x4();
-
-            var templates = new List<RoomTemplate>
-            {
-                square2x2,
-                square3x3,
-                rect2x3,
-                rect2x4,
-                square1x1,
-                rect1x2,
-                rect1x3,
-                rect1x4,
-                angle3x4,
-            };
-
-            foreach (var template in templates)
-            {
-                foreach (var cell in template.Cells.Array)
-                {
-                    cell?.SetCollectableGroup("Default");
-                }
-            }
-
-            templateGroups.Add("Rooms",
-                square2x2.UniqueVariations(),
-                square3x3.UniqueVariations(),
-                rect2x3.UniqueVariations(),
-                rect2x4.UniqueVariations());
-
-            templateGroups.Add("Paths",
-                square1x1.UniqueVariations(),
-                rect1x2.UniqueVariations(),
-                rect1x3.UniqueVariations(),
-                rect1x4.UniqueVariations(),
-                angle3x4.UniqueVariations());
-
             var expected = new List<int> { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
             collectableGroups.Add("Default", expected);
+
             var generator = new LayoutGenerator();
             var random = new RandomSeed(12345);
             var layout = generator.Generate(1, graph, templateGroups, random);
--- src/ManiaMap.Tests/TestGenerationPipeline.cs
+++ src/ManiaMap.Tests/TestGenerationPipeline.cs
@@ -11,55 +11,11 @@ namespace MPewsey.ManiaMap.Tests
         {
             var random = new RandomSeed(12345);
             var graph = Samples.GraphLibrary.BigGraph();
-            var templateGroups = new TemplateGroups();
+            var templateGroups = Samples.BigLayoutSample.BigLayoutTemplateGroups();
             var collectableGroups = new CollectableGroups();
+            collectableGroups.Add("Default", 1, 2, 3, 4, 5, 6, 7, 8, 9, 10);
 
-            var square2x2 = Samples.TemplateLibrary.Squares.Square2x2Template();
-            var square3x3 = Samples.TemplateLibrary.Squares.Square3x3Template();
-            var rect2x3 = Samples.TemplateLibrary.Rectangles.Rectangle2x3Template();
-            var rect2x4 = Samples.TemplateLibrary.Rectangles.Rectangle2x4Template();
-
-            var square1x1 = Samples.TemplateLibrary.Squares.Square1x1Template();
-            var rect1x2 = Samples.TemplateLibrary.Rectangles.Rectangle1x2Template();
-            var rect1x3 = Samples.TemplateLibrary.Rectangles.Rectangle1x3Template();
-            var rect1x4 = Samples.TemplateLibrary.Rectangles.Rectangle1x4Template();
-            var angle3x4 = Samples.TemplateLibrary.Angles.Angle3x4();
-
-            var templates = new List<RoomTemplate>
-            {
-                square2x2,
-                square3x3,
-                rect2x3,
-                rect2x4,
-                square1x1,
-                rect1x2,
-                rect1x3,
-                rect1x4,
-                angle3x4,
-            };
-
-            foreach (var template in templates)
-            {
-                foreach (var cell in template.Cells.Array)
-                {
-                    cell?.SetCollectableGroup("Default");
-                }
-            }
-
-            templateGroups.Add("Rooms",
-                square2x2.UniqueVariations(),
-                square3x3.UniqueVariations(),
-                rect2x3.UniqueVariations(),
-                rect2x4.UniqueVariations());
-
-            templateGroups.Add("Paths",
-                square1x1.UniqueVariations(),
-                rect1x2.UniqueVariations(),
-                rect1x3.UniqueVariations(),
-                rect1x4.UniqueVariations(),
-                angle3x4.UniqueVariations());
-
-            var map = new Dictionary<string, object>
+            var args = new Dictionary<string, object>
             {
                 { "LayoutId", 1 },
                 { "LayoutGraph", graph },
@@ -69,7 +25,7 @@ namespace MPewsey.ManiaMap.Tests
             };
 
             var pipeline = GenerationPipeline.CreateDefaultPipeline();
-            var results = pipeline.Generate(map);
+            var results = pipeline.Generate(args);
             Assert.IsTrue(results.ContainsKey("Layout"));
             Assert.IsNotNull(results["Layout"] as Layout);
         }
