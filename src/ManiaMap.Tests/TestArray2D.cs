82fe7e0ac44652d41544aff917541ee7ecf19786
Matt Pewsey <23442063+mpewsey@users.noreply.github.com>
2022-10-31 20:43:54 -0400

    Update TestArray2D.cs

--- src/ManiaMap.Tests/TestArray2D.cs
+++ src/ManiaMap.Tests/TestArray2D.cs
@@ -15,6 +15,7 @@ namespace MPewsey.ManiaMap.Tests
                 { 1, 2, 3 },
                 { 4, 5, 6 },
                 { 7, 8, 9 },
+                { 10, 11, 12 },
             };
 
             Serialization.SaveXml(path, array);
