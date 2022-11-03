35ed5c684b5d38bb683adbe16dd1cedb0d1b1cee
mpewsey <mpewsey@users.noreply.github.com>
2022-11-03 01:24:23 +0000

    deploy: 9a5a0008201e8fd2db38b2b68891d70698a04714

--- dir_6f264f0054ecfc50cf6599fcd7391e71.js
+++ dir_6f264f0054ecfc50cf6599fcd7391e71.js
@@ -1,5 +1,6 @@
 var dir_6f264f0054ecfc50cf6599fcd7391e71 =
 [
+    [ "__Docs__.cs", "_serialization_2_____docs_____8cs_source.html", null ],
     [ "Cryptography.cs", "_cryptography_8cs_source.html", null ],
     [ "JsonSerialization.cs", "_json_serialization_8cs_source.html", null ],
     [ "JsonWriterSettings.cs", "_json_writer_settings_8cs_source.html", null ],
