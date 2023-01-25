6eff5def8b09d3426bd75b44adfb7fbae8a4e5f8
Matt Pewsey <23442063+mpewsey@users.noreply.github.com>
2023-01-24 22:39:19 -0500

    Update Constants.cs

--- src/ManiaMap/Constants.cs
+++ src/ManiaMap/Constants.cs
@@ -1,7 +1,13 @@
 ﻿namespace MPewsey.ManiaMap
 {
-    internal class Constants
+    /// <summary>
+    /// Contains library constants.
+    /// </summary>
+    internal static class Constants
     {
+        /// <summary>
+        /// The data contract namespace.
+        /// </summary>
         internal const string DataContractNamespace = "https://github.com/mpewsey/ManiaMap";
     }
 }
