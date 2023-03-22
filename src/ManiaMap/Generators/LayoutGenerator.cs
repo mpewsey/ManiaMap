ea23d9199ea1113d685fa447d04c67c5bc207347
Matt Pewsey <23442063+mpewsey@users.noreply.github.com>
2023-03-22 12:35:08 -0400

    Change door code to flags

--- src/ManiaMap/Generators/LayoutGenerator.cs
+++ src/ManiaMap/Generators/LayoutGenerator.cs
@@ -382,7 +382,7 @@ namespace MPewsey.ManiaMap.Generators
         /// <param name="code">The door code.</param>
         /// <param name="direction">The edge direction.</param>
         private bool AddRoom(IRoomSource source,
-            Uid fromRoomId, int code, EdgeDirection direction)
+            Uid fromRoomId, DoorCode code, EdgeDirection direction)
         {
             var fromRoom = Layout.Rooms[fromRoomId];
             var z = source.Z - fromRoom.Position.Z;
@@ -430,8 +430,8 @@ namespace MPewsey.ManiaMap.Generators
         /// <param name="aheadCode">The ahead door code.</param>
         /// <param name="aheadDirection">The ahead edge direction.</param>
         private bool InsertRoom(IRoomSource source,
-            Uid backRoomId, int backCode, EdgeDirection backDirection,
-            Uid aheadRoomId, int aheadCode, EdgeDirection aheadDirection)
+            Uid backRoomId, DoorCode backCode, EdgeDirection backDirection,
+            Uid aheadRoomId, DoorCode aheadCode, EdgeDirection aheadDirection)
         {
             var backRoom = Layout.Rooms[backRoomId];
             var aheadRoom = Layout.Rooms[aheadRoomId];
