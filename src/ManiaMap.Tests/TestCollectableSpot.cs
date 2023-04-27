597421726fceacbaea8d6ebd6c71764243ac2e0d
Matt Pewsey <23442063+mpewsey@users.noreply.github.com>
2023-04-27 10:19:21 -0400

    Add tests

--- src/ManiaMap.Tests/TestCollectableSpot.cs
+++ src/ManiaMap.Tests/TestCollectableSpot.cs
@@ -1,7 +1,7 @@
 ﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
 using MPewsey.Common.Mathematics;
 
-namespace MPewsey.ManiaMap.Generators.Tests
+namespace MPewsey.ManiaMap.Tests
 {
     [TestClass]
     public class TestCollectableSpot
@@ -11,6 +11,20 @@ namespace MPewsey.ManiaMap.Generators.Tests
         {
             var spot = new CollectableSpot(new Vector2DInt(2, 3), "Default", 1);
             Assert.IsTrue(spot.ToString().StartsWith("CollectableSpot("));
+        }
+
+        [TestMethod]
+        public void TestValuesAreEqual()
+        {
+            var spot1 = new CollectableSpot(new Vector2DInt(2, 3), "Default", 1);
+            var spot2 = new CollectableSpot(new Vector2DInt(2, 3), "Default", 1);
+
+            Assert.IsTrue(CollectableSpot.ValuesAreEqual(null, null));
+            Assert.IsTrue(CollectableSpot.ValuesAreEqual(spot1, spot1));
+            Assert.IsTrue(CollectableSpot.ValuesAreEqual(spot2, spot2));
+            Assert.IsFalse(CollectableSpot.ValuesAreEqual(spot1, null));
+            Assert.IsFalse(CollectableSpot.ValuesAreEqual(null, spot1));
+            Assert.IsTrue(CollectableSpot.ValuesAreEqual(spot1, spot2));
         }
     }
 }
